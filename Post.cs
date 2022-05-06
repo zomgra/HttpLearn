956e701038f7fd43953949ac4c723b13a1d36b0a
zomgra <hiban.com@gmail.com>
2022-05-06 19:49:26 +0300

    Full project
    Работает отлично, берет 3 рандомные статьи и вводит их в консоль

---
+++ Post.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HttpLearn
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace HttpLearn
+{
+    class MainClass
+    {
+        private static string endPoint = "https://jsonplaceholder.typicode.com/posts";
+
+        public static void Main(string[] args)
+        {
+
+            var _posts = JsonConvert.DeserializeObject<List<Post>>(MakeRequest());
+
+            if (_posts == null) return;
+            for (int i = 0; i < 3; i++)
+            {
+                Random r = new Random();
+                int rInt = r.Next(0, _posts.Count);
+                Console.WriteLine(_posts[rInt].Id);
+                Console.WriteLine(_posts[rInt].Title);
+                Console.WriteLine(_posts[rInt].Body);
+                Console.WriteLine();
+            }
+        }
+        public static string MakeRequest()
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                return stream.ReadToEnd();
+            }
+        }
+    }
+    
+}
